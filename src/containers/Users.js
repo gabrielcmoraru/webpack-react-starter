7b5a53b1d289c3794ef66d83b9d75ba731e443e3
Gabriel Moraru <gabriel@velocitypartners.com>
2019-10-28 13:37:12 +0000

    create basic react app

---
+++ src/containers/Users.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import PizzaImage from '../components/PizzaImage/PizzaImage'
+
+export default class Users extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The Users</h1>
+                <p>The content of crazzy people</p>
+            </div>
+        )
+    }
+}
